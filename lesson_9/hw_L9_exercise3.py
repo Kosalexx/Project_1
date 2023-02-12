a044911087030b8282e50a6fe79d7d0529fead12
kosalexx <kostkost30@gmail.com>
2023-02-13 01:48:32 +0300

    Code edits. Bug fixes.

--- lesson_9/hw_L9_exercise3.py
+++ lesson_9/hw_L9_exercise3.py
@@ -10,20 +10,27 @@
 """
 
 
-def count_letter(text: str) -> int:
+def count_letter(text: str, sym: str) -> int:
     """Counts how many times a letter appears in a text."""
-    letter = input('Input a letter: ')
-    # version 1: первый пришедший в голову вариант
+
+    # version 1:
     # counter = 0
     # for symbols in text:
-    #     if symbols.lower() == letter.lower():
+    #     if symbols.lower() == sym.lower():
     #         counter += 1
-    # version 2: вспомнил про метод count()
-    counter = text.count(letter.lower()) + text.count(letter.upper())
-    result = f'The letter appears {counter} times in the text.'
-    return result
+    # version 2: with method count()
+    counter = text.count(sym.lower()) + text.count(sym.upper())
+    return counter
 
 
-text_file = open('text_file(ex3).txt', 'r')
-some_text = text_file.read()
-print(count_letter(some_text))
+# text_file = open('text_file(ex3).txt', 'r')
+# some_text = text_file.read()
+# print(count_letter(some_text))
+
+
+result = 0
+with open('text_file(ex3).txt') as file:
+    letter = input('Input a letter: ')
+    for line in file:
+        result += count_letter(line, letter)
+print(f'Entered letter "{letter}" appears {result} times in text.')
--- lesson_9/hw_L9_exercise4.py
+++ lesson_9/hw_L9_exercise4.py
@@ -16,27 +16,24 @@ from math import sqrt
 def distance(point_1: tuple, point_2: tuple, *points: tuple) -> float:
     """Returns the distance between points with passed coordinates. """
 
-    # формула: d**2 = (x2-x1)**2 + (y2-y1)**2
-    # (x1, y1) - координаты 1-й точки; (x2, y2) - координаты 2-й точки
     x1, y1 = point_1[0], point_1[1]
     x2, y2 = point_2[0], point_2[1]
-    # расстояние между первыми двумя точками
     result = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    x1, y1 = x2, y2  # обновляю (x1, y1) для дальнейших вычислений
+    x1, y1 = x2, y2
     for new_point in points:
         x2 = new_point[0]
         y2 = new_point[1]
         result += sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        x1, y1 = x2, y2  # обновляю (x1, y1) для дальнейших вычислений
+        x1, y1 = x2, y2
 
-    return f'Длина маршрута между введенными точками: {round(result, 2)}'
+    return f'The distance between the selected points: {round(result, 2)}'
 
 
-number_points = int(input('Введите количество точек(>= 2): '))
+number_points = int(input('Enter the number of points (>= 2): '))
 points_list = []
 for _ in range(number_points):
-    point = tuple(int(num) for num in input('Введите координаты '
-                                            'точки (через пробел): ').split())
+    point = tuple(int(num) for num in input('Enter coordinates of points ('
+                                            'separated by space): ').split())
     points_list.append(point)
 
 print(distance(*points_list))
