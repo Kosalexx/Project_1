81836fcf7d8ef926000605ec3de6d0a21fd485fb
kosalexx <kostkost30@gmail.com>
2023-02-16 19:42:14 +0300

    Code edits.

--- lesson_9/homework2.py
+++ lesson_9/homework2.py
@@ -17,13 +17,14 @@ def find_by_name(name: str) -> list:
         file_reader = csv.DictReader(r_file, delimiter=',')
 
         for row in file_reader:
-            final_dict = {}
-            if name in row['Name'].lower() or row['Name'].lower() in name:
-                final_dict['Name'] = row['Name']
-                final_dict['Symbol'] = row['Symbol']
-                final_dict['Sector'] = row['Sector']
-                final_dict['Stock price'] = float(row['Price'])
-                answer.append(final_dict)
+            if name in row.get('Name').lower():
+                answer.append({
+                    "Name": row.get("Name"),
+                    "Symbol": row.get("Symbol"),
+                    "Sector": row.get("Sector"),
+                    "Stock Price": row.get("Price")
+                    }
+                )
     if len(answer) == 0:
         return "There aren't any companies that match the request."
     else:
@@ -39,15 +40,14 @@ def find_by_symbol(symbol: str) -> list:
     with open("sp500.csv", encoding='utf-8') as r_file:
         file_reader = csv.DictReader(r_file, delimiter=',')
         for row in file_reader:
-            final_dict = {}
-            if symbol.upper() in row['Symbol'] or row['Symbol'] in  \
-                    symbol.upper():
-                final_dict['Name'] = row['Name']
-                final_dict['Symbol'] = row['Symbol']
-                final_dict['Sector'] = row['Sector']
-                final_dict['Stock price'] = float(row['Price'])
-                final_list.append(final_dict)
-
+            if symbol.upper() in row['Symbol']:
+                final_list.append({
+                    "Name": row.get("Name"),
+                    "Symbol": row.get("Symbol"),
+                    "Sector": row.get("Sector"),
+                    "Stock Price": row.get("Price")
+                    }
+                )
     if len(final_list) == 0:
         return "There aren't any companies that match the request."
     else:
@@ -64,11 +64,10 @@ def companies_by_sector(sector: str) -> list:
     with open("sp500.csv", encoding='utf-8') as r_file:
         file_reader = csv.DictReader(r_file, delimiter=',')
         for row in file_reader:
-            if row['Sector'].lower() in sector.lower() or \
-                    sector.lower() in row['Sector'].lower():
-                final_list.append(row['Name'])
-                if row['Sector'] not in correct_sector_name:
-                    correct_sector_name.append(row['Sector'])
+            if sector.lower() in row.get('Sector').lower():
+                final_list.append(row.get('Name'))
+                if row.get('Sector') not in correct_sector_name:
+                    correct_sector_name.append(row.get('Sector'))
 
     if len(final_list) >= 1 and len(correct_sector_name) == 1:
         print(f'You chose sector {correct_sector_name}:')
@@ -85,7 +84,7 @@ def calc_average_price() -> float:
         file_reader = csv.DictReader(r_file, delimiter=',')
         for row in file_reader:
             counter_of_companies += 1
-            total_price += float(row['Price'])
+            total_price += float(row.get('Price'))
     average_price = round((total_price / counter_of_companies), 2)
     return average_price
 
@@ -98,8 +97,8 @@ def top_ten_companies() -> list:
     with open("sp500.csv", encoding='utf-8') as r_file:
         file_reader = csv.DictReader(r_file, delimiter=',')
         for row in file_reader:
-            price_1 = float(row['Price'])
-            name_1 = row['Name']
+            price_1 = float(row.get('Price'))
+            name_1 = row.get('Name')
             tuple_price = (name_1, price_1)
             if len(final_list) == 0:
                 final_list.append(tuple_price)
@@ -130,42 +129,42 @@ def maybe_stop() -> bool:
     return flag
 
 
-start = """Choose the action from menu:
-1 - Find info by name
-2 - Find info by symbol
-3 - Get all companies by sector
-4 - Calculate average price
-5 - Get top 10 companies
-6 - Exit """
+start = ("Choose the action from menu:\n"
+         "1 - Find info by name\n"
+         "2 - Find info by symbol\n"
+         "3 - Get all companies by sector\n"
+         "4 - Calculate average price\n"
+         "5 - Get top 10 companies\n"
+         "6 - Exit ")
 
 flag = True
 while flag:
     print(start)
-    choice = int(input('Your choice: '))
-    if choice == 1:
+    choice = input('Your choice: ')
+    if choice == "1":
         company_name = input('Enter the name of company: ').lower()
         result = find_by_name(company_name)
         print(result)
         flag = maybe_stop()
-    elif choice == 2:
+    elif choice == "2":
         company_symbol = input('Enter the Symbol of company: ')
         result = find_by_symbol(company_symbol)
         print(result)
         flag = maybe_stop()
-    elif choice == 3:
+    elif choice == "3":
         sector_name = input('Enter name of sector: ')
         result = companies_by_sector(sector_name)
         print(result)
         flag = maybe_stop()
-    elif choice == 4:
+    elif choice == "4":
         result = calc_average_price()
         print(f'Average price: {result}')
         flag = maybe_stop()
-    elif choice == 5:
+    elif choice == "5":
         result = top_ten_companies()
         print(result)
         flag = maybe_stop()
-    elif choice == 6:
+    elif choice == "6":
         print('GOODBYE!')
         flag = False
     else:
