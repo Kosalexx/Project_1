1140435fbc58f6ae116c165d8c397c465f29fcd6
kosalexx <kostkost30@gmail.com>
2023-03-08 00:20:28 +0300

    Code Edits.

--- lesson_14/hw_L14_task3.py
+++ lesson_14/hw_L14_task3.py
@@ -3,47 +3,50 @@ class IncorrectDataError(Exception):
 
 
 class Element(object):
-    def __init__(self, data, nextval=None) -> None:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next_val = None
+
+    def _validate_data(self, data):
         if data not in range(1, 10000):
             raise IncorrectDataError("Data must be in range from 1 to 10000.")
-        self.data = data
-        self.next = nextval
 
-    # Get methods
-    def get_data(self):
+    @property
+    def data(self):
         """Gets the value of data."""
-        return self.data
+        return self._data
 
-    def get_next(self):
+    @property
+    def next_val(self):
         """Gets the value of next item of the Element."""
-        return self.next
+        return self._next
 
-    # Set methods
-    def set_data(self, data: int):
+    @data.setter
+    def data(self, value: int):
         """Sets the value of data."""
-        if data not in range(1, 10000):
-            raise IncorrectDataError("Data must be in range from 1 to 10000.")
-        self.data = data
+        self._validate_data(value)
+        self._data = value
 
-    def set_next(self, nextval):
+    @next_val.setter
+    def next_val(self, nextval):
         """Sets the value of next item of the Element."""
-        self.next = nextval
+        self._next = nextval
 
     # Comparison methods
     def __eq__(self, other) -> bool:
         if not isinstance(other, Element):
             raise ValueError("The second object must be Element.")
-        return self.get_data() == other.get_data()
+        return self.data == other.data
 
     def __gt__(self, other) -> bool:
         if not isinstance(other, Element):
             raise ValueError("The second object must be Element.")
-        return self.get_data() > other.get_data()
+        return self.data > other.data
 
     def __ge__(self, other) -> bool:
         if not isinstance(other, Element):
             raise ValueError("The second object must be Element.")
-        return self.get_data() >= other.get_data()
+        return self.data >= other.data
 
 
 class LinkedList(object):
@@ -55,7 +58,7 @@ class LinkedList(object):
         counter = 0
         while cur_element is not None:
             counter += 1
-            cur_element = cur_element.get_next()
+            cur_element = cur_element.next_val
         return counter
 
     def __iter__(self):
@@ -65,28 +68,28 @@ class LinkedList(object):
     def __next__(self):
         if self.cur_el is None:
             raise StopIteration
-        result = self.cur_el.get_data()
-        self.cur_el = self.cur_el.get_next()
+        result = self.cur_el.data
+        self.cur_el = self.cur_el.next_val
         return result
 
     def append(self, element: int) -> None:
         """ Adds element to the end of LinkedList."""
         new_element = Element(element)
         cur_element = self.head
-        if cur_element is None:
+        if self.head is None:
             self.head = new_element
             return
-        while cur_element.get_next() is not None:
-            cur_element = cur_element.get_next()
-        cur_element.set_next(new_element)
+        while cur_element.next_val is not None:
+            cur_element = cur_element.next_val
+        cur_element.next_val = new_element
 
     def println(self) -> None:
         """Prints the value of LinkedList as a list."""
         cur_element = self.head
         res_list = []
         while cur_element is not None:
-            res_list.append(cur_element.get_data())
-            cur_element = cur_element.get_next()
+            res_list.append(cur_element.data)
+            cur_element = cur_element.next_val
         print(res_list)
 
     def reverse(self) -> None:
@@ -95,8 +98,8 @@ class LinkedList(object):
         cur_el = self.head
         next_el = None
         while cur_el is not None:
-            next_el = cur_el.get_next()
-            cur_el.set_next(prev_el)
+            next_el = cur_el.next_val
+            cur_el.next_val = prev_el
             prev_el = cur_el
             cur_el = next_el
         self.head = prev_el
@@ -116,7 +119,7 @@ print(el1 >= el2)
 print(el1 >= el2)
 
 my_list = LinkedList()
-my_list.append(1)
+my_list.append(2)
 my_list.append(2)
 my_list.append(6)
 my_list.append(20)
